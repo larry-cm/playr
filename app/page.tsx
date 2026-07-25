3e4060da8316b9edeb7e927d0e6889cbf706b588
larry-cm <xetisimo@gmail.com>
2026-07-24 22:28:53 -0500

    pagina login

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,73 @@
-import Image from "next/image";
-
+"use client";
+import Card from "@ui/card";
+import { loginAction } from "@action/login-action";
+import { useActionState } from "react";
+import type { LoginState } from "@action/login-action";
+const initialState: LoginState = { success: false, errors: {} };
 export default function Home() {
+  const [state, action, isLoading] = useActionState(loginAction, initialState);
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      {state && state.success && (
+        <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+          <strong className="font-bold">¡Éxito!</strong>
+          <span className="block sm:inline"> Has iniciado sesión correctamente.</span>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      )}
+
+      {state && state.message && (
+        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+          <span className="block sm:inline">{state.message}</span>
         </div>
-      </main>
-    </div>
+      )}
+
+      <article className="flex flex-col gap-4 w-full max-w-md">
+        <Card>
+          <h1 className="text-2xl font-bold text-center mb-4">Inicio de sesión</h1>
+          <p>¡Bienvenido de vuelta! Por favor, ingresa tus credenciales para acceder a tu cuenta.</p>
+
+          <form className="flex flex-col gap-4 mt-4" action={action}>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="email" className="font-semibold block w-fit">Correo electrónico</label>
+              <input type="email" id="email" name="email" placeholder="Ingresa tu correo electrónico" className="border border-gray-300 rounded-md p-2" />
+              <div>
+                {
+                  state && state.errors && state.errors.email &&
+                  state.errors.email.map((error, index) => (
+                    <p key={index} className="text-red-500 text-sm">{error}</p>
+                  ))
+                }
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="contraseña" className="font-semibold block w-fit">Contraseña</label>
+              <input type="password" id="contraseña" name="contraseña" placeholder="Ingresa tu contraseña" className="border border-gray-300 rounded-md p-2" />
+
+              <div>
+                {
+                  state && state.errors && state.errors.password &&
+                  state.errors.password.map((error, index) => (
+                    <p key={index} className="text-red-500 text-sm">{error}</p>
+                  ))
+                }
+              </div>
+
+            </div>
+
+            <div className="flex items-center gap-2">
+              <input type="checkbox" id="recordar" name="recordar" className="w-4 h-4" />
+              <label htmlFor="recordar" className="font-semibold">Recordarme</label>
+            </div>
+
+            <button type="submit" className="bg-blue-500 text-white rounded-md p-2 mt-4 hover:bg-blue-600 transition-colors">
+              {isLoading ? "Cargando..." : "Iniciar sesión"}
+            </button>
+          </form>
+        </Card>
+      </article>
+
+
+    </main>
   );
 }

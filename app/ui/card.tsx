efa97c2a177ab64869780c135d36a09f90fe7d81
larry-cm <xetisimo@gmail.com>
2026-07-25 12:58:58 -0500

    fix

--- app/ui/card.tsx
+++ app/ui/card.tsx
@@ -6,7 +6,7 @@ export default function Card({
     children: React.ReactNode;
 }>) {
     return (
-        <section className="bg-white/[0.03] backdrop-blur-xl border border-white/[0.06] rounded-2xl shadow-2xl p-8 w-full">
+        <section className="bg-white/3 backdrop-blur-xl border border-white/6 rounded-2xl shadow-2xl p-8 w-full">
             {children}
         </section>
     )

72fdda339966207a01e76645cedbd6ad7820dd3b
larry-cm <xetisimo@gmail.com>
2026-07-25 16:51:09 -0500

    chore: :wrench: infraestructura y middlewares

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,16 @@
-import { type NextRequest } from "next/server"
+import { type NextRequest, NextResponse } from "next/server"
 import { createClient } from "@/app/lib/supabase/middleware"
 
-export async function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest, next: (request: NextRequest) => Promise<Response>) {
   const { supabase, supabaseResponse } = createClient(request)
 
-  await supabase.auth.getUser()
+  const { data, error } = await supabase.auth.getUser()
+  const pathManager = request.nextUrl.pathname.startsWith("/administrar")
+  const isAuthorized = data.user?.role === "authenticated"
 
+  if (pathManager && !isAuthorized) {
+    return NextResponse.redirect(new URL("/", request.url))
+  }
   return supabaseResponse
 }
 
